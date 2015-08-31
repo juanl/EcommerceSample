86d58f4524f40b93988bda07ee01234a18ee3177
golmedo <gustavo.olmedo.formosa@gmail.com>
2015-08-31 18:33:43 -0300

    resolviendo conflictos de merge

---
+++ Servicios/ProductoServicioFake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Entidades;
+
+namespace Servicios
+{
+    class ProductoServicioFake : IProductoServicio
+    {
+        public ResultadoOperacion BorrarProducto(Producto producto)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public ResultadoOperacion GuardarProducto(Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Producto> ObtenerPorCategoria(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Producto> ObtenerPorDescripcion(string cadena)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Producto ObtenerPorId(int productoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Producto> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
