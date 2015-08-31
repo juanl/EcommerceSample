b268960a806ddf8455603b13a780d072e7d8d68e
juanl <jladetto@invertironline.com>
2015-08-31 18:05:33 -0300

    servicios e infra

---
+++ Servicios/IProductoServicio.cs
@@ -0,0 +1,19 @@
+﻿using Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    interface IProductoServicio
+    {
+        IList<Producto> ObtenerPorDescripcion(string cadena);
+        IList<Producto> ObtenerPorCategoria(Categoria categoria);
+        Producto ObtenerPorId(int productoId);
+        IList<Producto> ObtenerTodos();
+        ResultadoOperacion GuardarProducto(Producto producto);
+        ResultadoOperacion BorrarProducto(Producto producto);
+    }
+}
---
+++ Servicios/ResultadoOperacion.cs
@@ -0,0 +1,14 @@
+﻿namespace Servicios
+{
+    public enum Resultado
+    {
+        ok=1,
+        Error=2
+    }
+
+    public class ResultadoOperacion
+    {
+        public string Mensaje { get; set; }
+        public Resultado Resultado { get; set; }
+    }
+}
