3e8e3bf16a4ee0805344c7d1844f6cbfd7070814
juanl <jladetto@invertironline.com>
2015-09-07 18:44:34 -0300

    carrito de compra desde session

--- Dominio/Entidades/ElementoCarrito.cs
+++ Dominio/Entidades/ElementoCarrito.cs
@@ -1,21 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dominio.Entidades
 {
-  public class ElementoCarrito
-  {
-    [Key]
-    public string ElementoId { get; set; }
+    public class ElementoCarrito : IComparable
+    {
+        [Key]
+        public string ElementoId { get; set; }
 
-    public string CarritoId { get; set; }
+        public string CarritoId { get; set; }
 
-    public int Cantidad { get; set; }
+        public int Cantidad { get; set; }
 
-    public System.DateTime FechaCreado { get; set; }
+        public System.DateTime FechaCreado { get; set; }
 
-    public int ProductoId { get; set; }
+        public int ProductoId { get; set; }
 
-    public virtual Producto Producto { get; set; }
+        public virtual Producto Producto { get; set; }
 
-  }
+        public int CompareTo(object obj)
+        {
+            var producto = (ElementoCarrito)obj;
+            return this.ProductoId.CompareTo(producto.ProductoId);
+        }
+    }
 }
--- Servicios/ProductoServicioFake.cs
+++ Servicios/ProductoServicioFake.cs
@@ -5,18 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using Dominio.Entidades;
 using Infraestructura.Context;
+using Servicios.DTOs;
 
 namespace Servicios
 {
-    class ProductoServicioFake : IProductoServicio
+    public class ProductoServicioFake : IProductoServicio
     {
         Contexto _context = new Contexto();
+
+        public ResultadoOperacion BorrarProducto(ProductoDTO _productoDTO)
+        {
+            throw new NotImplementedException();
+        }
+
         public ResultadoOperacion BorrarProducto(Producto producto)
         {
             throw new NotImplementedException();
         }
 
+        public ResultadoOperacion EditarProducto(ProductoDTO _productoDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultadoOperacion GuardarProducto(ProductoDTO _productoDTO)
+        {
+            throw new NotImplementedException();
+        }
+
         public ResultadoOperacion GuardarProducto(Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ProductoDTO> ObtenerPorCategoria(CategoriaDTO categoria)
         {
             throw new NotImplementedException();
         }
@@ -37,6 +59,21 @@ namespace Servicios
         }
 
         public IList<Producto> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<ProductoDTO> IProductoServicio.ObtenerPorDescripcion(string cadena)
+        {
+            throw new NotImplementedException();
+        }
+
+        ProductoDTO IProductoServicio.ObtenerPorId(int productoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<ProductoDTO> IProductoServicio.ObtenerTodos()
         {
             throw new NotImplementedException();
         }
