58f26ffc28a99200307a5b84c95e3357ef354665
GOlmedoFormosa <gustavo.olmedo.formosa@gmail.com>
2015-09-07 18:47:13 -0300

    Controller de carrito compra

---
+++ WebApiProject/Controllers/CarritoCompraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiProject.Controllers
+{
+    public class CarritoCompraController : ApiController
+    {
+        //public void AgregarProducto(ElementoCarrito item) { }
+        //public void EliminarProducto(ElementoCarrito item) { }
+        //public void AumentarCantidad(ElementoCarrito item) { }
+        //public IList<ElementeoCarrito> ListarProductos(ElementoCarrito item) { }
+    }
+}
